a095741c9588860dd9325ffa9029600532690299
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-26 22:54:04 +0530

    Install dependancies and create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express               = require("express"),
+      app                   = express(),
+      cors                  = require("cors"),
+      bodyParser            = require("body-parser"),
+      mongoose              = require("mongoose"),
+      methodOverride        = require("method-override");
+      middleware            = require("./middlwares/index");
+
+require("dotenv").config();
+
+//ROUTES
+const indexRoute         = require("./routes/index");
+    //   noteRoute          = require("./routes/note");
+
+mongoose.connect(process.env.DATABASEURL,{ useUnifiedTopology: true ,useNewUrlParser:true});
+mongoose.set("useFindAndModify",false);
+mongoose.set("useCreateIndex",true);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set("view engine","ejs");
+app.use(methodOverride("_method"));
+
+middleware(app);
+
+app.use("/",indexRoute);
+// app.use("/note",noteRoute);
+
+app.listen(5000)
+{
+    console.log("Server has started");
+}
