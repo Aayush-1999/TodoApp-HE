cc366a3850022afedd1fb421233783a6238283e2
Aayush-1999 <aayushaggarwal2007@gmail.com>
2020-05-31 01:17:55 +0530

    create Routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import {Route , Redirect, Switch} from 'react-router-dom';
+import { connect } from 'react-redux';
+import LoginEmail from './Containers/Login/Email/LoginEmail';
+import LoginPassword from './Containers/Login/Password/LoginPassword';
+import SignUp from './Containers/Signup/Signup';
+import Layout from './hoc/Layout/Layout';
+import Home from './Containers/Home/Home';
+import Auxiliary from './hoc/Auxiliary/Auxiliary';
+import * as actions from './store/actions/index';
 
 class App extends Component{
+  componentDidMount(){
+    this.props.onCheckAuthState()
+  }
+
   render(){
+    let routes=(
+      <Switch>
+        <Route path="/" exact><Redirect to="/signin" /></Route>
+        <Route path="/signin" exact component={LoginEmail} />
+        <Route path="/signin/pwd" exact component={LoginPassword} />
+        <Route path="/signup" exact component={SignUp} />
+        <Redirect to="/" />
+      </Switch>
+    )
+    
+    if(this.props.isAuthenticated){
+      routes=(
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Redirect to="/" />
+          </Switch>
+        </Layout>
+      )
+    }
     return(
-      <h1>App</h1>
+        <Auxiliary>
+          {routes}
+        </Auxiliary>
     )
   }
 }
 
-export default App;
+const mapStateToProps=state=>{
+  return{
+    isAuthenticated:state.auth.authenticated
+  }
+}
+
+const mapDispatchToProps=dispatch=>{
+  return{
+    onCheckAuthState:()=>dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
